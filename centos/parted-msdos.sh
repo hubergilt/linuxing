48d73350557231030114ae6f5da49f4042872147
Huber Paul Gilt Lopez <hubergilt@hotmail.com>
2019-11-01 03:13:34 -0500

    Using parted to format disk with gtp partion table

--- centos/parted-msdos.sh
+++ centos/parted-msdos.sh
@@ -19,11 +19,11 @@ done;
 parted /dev/sdb mkpart extended 4G 20G
 
 #define numer of partitions
-PARTS=$(seq 5 21)
+PARTS=$(seq 4 20)
 #loop for make partition, format, create dir and mount
 for PART in ${PARTS};
-do parted /dev/sdb mkpart logical ext4 $((${PART}-1))G ${PART}G;
-   mkfs.ext4 /dev/sdb${PART};
-   [ ! -d /data$((${PART}-1)) ] && mkdir /data$((${PART}-1));
-   mount /dev/sdb${PART} /data$((${PART}-1));
+do parted /dev/sdb mkpart logical ext4 ${PART}G $((${PART}+1))G;
+   mkfs.ext4 /dev/sdb$((${PART}+1));
+   [ ! -d /data${PART} ] && mkdir /data${PART};
+   mount /dev/sdb$((${PART}+1)) /data${PART};
 done;
