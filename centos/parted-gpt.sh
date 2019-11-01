2237e2e01d5141caabaea2f7a496fa594d66130c
Huber Paul Gilt Lopez <hubergilt@hotmail.com>
2019-11-01 08:46:34 -0500

    Using parted to format disk with gtp partion table

--- centos/parted-gpt.sh
+++ centos/parted-gpt.sh
@@ -6,7 +6,7 @@ umount /data*
 wipefs -a /dev/sdb
 #create partition table
 parted /dev/sdb mktable gpt
-#define numer of partitions
+#define number of partitions
 PARTS=$(seq 1 20)
 #loop for make partition, format, create dir and mount
 for PART in ${PARTS};
