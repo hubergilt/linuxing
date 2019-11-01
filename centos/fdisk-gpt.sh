e47b619e36a05de197025583a0fa530c49d59b5a
Huber Paul Gilt Lopez <hubergilt@hotmail.com>
2019-11-01 09:04:06 -0500

    Using fdisk to format disk with gtp partion table

--- centos/fdisk-gpt.sh
+++ centos/fdisk-gpt.sh
@@ -13,7 +13,7 @@ EOF
 
 #define number of partitions
 PARTS=$(seq 1 20)
-#loop for make partition, format, create dir and mount
+#loop for make partition
 for PART in ${PARTS};
 do fdisk /dev/sdb << EOF
 n
@@ -22,4 +22,10 @@ ${PART}
 +1GB
 w
 EOF
+
+#and format, create dir and mount
+mkfs.ext4 /dev/sdb${PART};
+[ ! -d /data${PART} ] && mkdir /data${PART};
+mount /dev/sdb${PART} /data${PART};
+
 done;
